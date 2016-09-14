364616f9511fe3ccd365fbe7b366e33794a6a649
Tomas Petrak <tomaspetrak@Tomass-MacBook-Pro.local>
2016-09-14 11:01:47 +0200

    timeline for humidity added

--- src/main/resources/static/js/fetchHumidity.js
+++ src/main/resources/static/js/fetchHumidity.js
@@ -19,12 +19,24 @@ function data(offset) {
     for (var x = 0; x <= 360; x += 10) {
         var v = (offset + x) % 360;
         ret.push({
-            x: x,
+            x:  getTimeString(x),
             y: humidityValues[Math.floor(x/10) + nReloads]
            // z: Math.cos(Math.PI * v / 180).toFixed(4)
         });
     }
     return ret;
+}
+
+
+function getTimeString(x){
+    var d = new Date();
+    console.log(d);
+    var timeString = d.getHours() + ":";
+    var minutes = d.getMinutes() + Math.floor((d.getSeconds() + x/10)/60);
+    timeString += (minutes>10)? minutes + ":" : "0" + minutes + ":";
+    var seconds = (d.getSeconds() + x/10) % 60;
+    timeString += (seconds>10)? seconds : "0" + seconds;
+    return timeString;
 }
 
 function update() {
@@ -35,6 +47,7 @@ function update() {
 }
 
 function getCurrentDummyHumidity(){
+    // return Math.floor(Math.random()*100); // Dummy data
     return wateringData.deviceData.humidity;
 }
 
